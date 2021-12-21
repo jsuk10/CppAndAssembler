f29362be34d9c31ff2e7957501d0deacf5affb49
jsuk10 <jsuk10@naver.com>
2021-12-21 16:32:10 +0900

    Boxing UnBoxing

---
+++ CS/Basic/Code/boxing5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// CompareTo : IComparable, IComparable<T>
+// Equals    : object, IEquatable<T>
+/*
+interface IEquatable<T>
+{
+    bool Equals(T other)
+}
+*/
+
+struct Point : IEquatable<Point>
+{
+    private int x;
+    private int y;
+    public Point(int xPos, int yPos)
+    {
+        x = xPos;
+        y = yPos;
+    }
+    public bool Equals(Point pt)
+    {
+        Console.WriteLine("Point");
+        return x == pt.x && y == pt.y;
+    }
+    public override bool Equals(object obj)
+    {
+        Console.WriteLine("object");
+        Point pt = (Point)obj;
+        return Equals(pt);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point p1 = new Point(1, 1);
+        object p2 = new Point(1, 1);
+        Point p3 = new Point(1, 1);
+
+        Console.WriteLine(p1.Equals(p2));   //objcet에서 Point로
+        Console.WriteLine(p1.Equals(p3));   //point출력
+    }
+}
+
