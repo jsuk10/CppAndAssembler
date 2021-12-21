23e907dd121cf4c2fea4e4d4735f1a7a0294ca70
jsuk10 <jsuk10@naver.com>
2021-12-21 11:09:29 +0900

    한글 표기 수정

--- CS/Basic/Code/class3.cs
+++ CS/Basic/Code/class3.cs
@@ -7,14 +7,14 @@ class Base
 }
 class Derived : Base
 {
-    //이렇게 해야 해당 메소드가 나온다.
-    // public override void Foo() { WriteLine("Derived foo"); } //가상 메소드 재정의
+    //이렇게 해야 해당 메서드가 나온다.
+    // public override void Foo() { WriteLine("Derived foo"); } //가상 메서드 재정의
 
 
     //아래는 Base foo가 호출된다.
     // public void Foo() { WriteLine("Derived foo"); }          
-    // public new void Foo() { WriteLine("Derived foo"); }      // 새로운 메소드를 만든것
-    // public virtual void Foo() { WriteLine("Derived foo"); }  // 또다른 가상 메소드 생성함.
+    // public new void Foo() { WriteLine("Derived foo"); }      // 새로운 메서드를 만든것
+    // public virtual void Foo() { WriteLine("Derived foo"); }  // 또다른 가상 메서드 생성함.
 }
 
 class Program
