542a5a0f908113425a67daba82c252311220bdd4
jsuk10 <jsuk10@naver.com>
2021-12-21 18:38:39 +0900

    CS 함수의 인자 전달 방식

---
+++ CS/Basic/Code/callby2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void inc1(int n)          // Call By Value
+    {
+        ++n;
+    }
+    public static void inc2(ref int n)      // Call By Ref
+    {
+        ++n;
+    }
+
+    public static void Main()
+    {
+        int n1 = 10;
+        inc1(n1);
+        Console.WriteLine(n1);          // 10 (Call By Value)
+
+        int n2 = 10;
+        inc2(ref n2);
+        Console.WriteLine(n2);          // 11 (Call By Ref)
+    }
+}
---
+++ CS/Basic/Code/callby4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Point
+{
+    public int x = 0;
+    public int y = 0;
+    public Point(int a, int b) { x = a; y = b; }
+}
+
+class Program
+{
+    public static void f1(Point p)      // Point p = p1
+    {
+        p.x = 2;                        // p1의 x는 2로 바뀜
+        p = new Point(5, 5);            // p가 가르키는 객체는 5가 되었으나 p1이 가르키는 곳은 그대로임
+    }
+    public static void Main()
+    {
+        Point p1 = new Point(1, 1);
+        f1(p1);
+        Console.WriteLine(p1.x);        //out 2
+        f2(ref p1);
+        Console.WriteLine(p1.x);        //out 5
+    }
+
+    public static void f2(ref Point p)  // ref Point p = refp1
+    {
+        p.x = 2;
+        p = new Point(5, 5);
+    }
+
+}
