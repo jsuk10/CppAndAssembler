24a93013450d358c2e6869aa3f8a815203aa78fb
jsuk10 <jsuk10@naver.com>
2021-12-21 17:59:07 +0900

    Boxing/UnBoxing 누락본

---
+++ CS/Basic/Code/boxing2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(10 < 20); // true
+
+        Console.WriteLine(10.CompareTo(20)); // -1
+        Console.WriteLine(10.CompareTo(10)); // 0
+        Console.WriteLine(10.CompareTo(5));  // 1
+
+        //Console.WriteLine("AAA" < "BBB"); // error
+        Console.WriteLine("AAA".CompareTo("BBB")); // -1
+
+
+    }
+}
