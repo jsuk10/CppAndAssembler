18d60ae9fa8e03fa1fd2e93a3671253b70b9eb9f
jsuk10 <jsuk10@naver.com>
2021-12-22 23:09:59 +0900

    Delegate 1

---
+++ C#/Basic/Code/delegate3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+delegate void FUNC(int arg);
+
+/*
+class FUNC : System.MulticastDelegate
+{
+    //....
+}
+*/
+
+class Program
+{
+    public static void Main()
+    {
+        FUNC f1 = new FUNC(foo);    // 정확한 표기법
+        f1.Invoke(10);              // 
+
+        FUNC f2 = foo;              // C#내부적으로 처리해줌
+        f2(10);                     // f2.Invoke(10)
+    }
+
+    public static void foo(int arg)
+    {
+        Console.WriteLine($"foo : {arg}");
+    }
+}
+
+
