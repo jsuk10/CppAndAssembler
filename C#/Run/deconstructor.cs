14af2ab13c48da964be24f6c1390d2908a61e012
jsuk10 <jsuk10@naver.com>
2021-12-28 18:28:41 +0900

    construct + Deconstructor

---
+++ C#/Run/deconstructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Point
+{
+    public int x;
+    public int y;
+    public Point(int a, int b) { x = a; y = b; }
+
+    public void Deconstruct(out int a, out int b)
+    {
+        a = x;
+        b = y;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Point point = new Point(1, 2);
+
+        int a = point.x;
+        int b = point.y;
+
+        var (a1, b1) = point;
+
+        point.Deconstruct(out int a2, out int b2);
+
+        Console.WriteLine($"{a1}, {b1}");
+    }
+}
--- C#/Run/makeil.bat
+++ C#/Run/makeil.bat
@@ -1,6 +1,8 @@
 @csc %1
 @ildasm /out=%~n1.il %~n1.exe
-@del %~n1.exe
-@del %~n1.res
 @copy %1 ..\Intermediate\Code
 @copy %~n1.il ..\Intermediate\Code
+@del %~n1.res
+@del %~n1.il
+@%~n1.exe
+@del %~n1.exe
