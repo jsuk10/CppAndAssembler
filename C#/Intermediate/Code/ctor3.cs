a54021bc834ef89ae8a3f21a7a7eb36e0d1bc2e3
jsuk10 <jsuk10@naver.com>
2021-12-28 17:48:47 +0900

    생성자 1

---
+++ C#/Intermediate/Code/ctor3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+class Base
+{
+    // public Base() { WriteLine("Base()"); }
+    public Base(int n) { WriteLine("Base(int)"); }
+}
+class Derived : Base
+{
+    public Derived() : base(0) { WriteLine("Derived()"); }
+    // 아래 두줄 코드는 기능이 동일하다.
+    // public Derived(int n) { WriteLine("Derived(int)"); }
+    // public Derived(int n) : base(){ WriteLine("Derived(int)"); }
+    // 기반 클래스에도 인자를 넣고 싶으면 아래와 같이 하면된다.
+    public Derived(int n) : base(n) { WriteLine("Derived(int)"); }
+}
+class Program
+{
+    public static void Main()
+    {
+        Derived d = new Derived(0);
+    }
+}
---
+++ C#/Intermediate/Code/value_type_ctor1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class CPoint
+{
+    public int x;
+    public int y;
+    public CPoint(int a, int b) { x = a; y = b; }
+}
+struct SPoint
+{
+    public int x;
+    public int y;
+    //public SPoint() { }       //만들면 error
+    public SPoint(int a, int b) { x = a; y = b; }
+}
+class Program
+{
+    public static void Main()
+    {
+        CPoint cp1 = new CPoint(1, 2);  // ok
+        //CPoint cp2 = new CPoint();     // error 인자 없는 생성자가 없음.
+        SPoint sp1 = new SPoint(1, 2);  //ok
+        SPoint sp2 = new SPoint();      //ok? ok
+    }
+}
