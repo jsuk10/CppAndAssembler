f173cc1c494d763d782a4c3b5469736a83bc0a8a
jsuk10 <jsuk10@naver.com>
2021-12-29 14:21:55 +0900

    pattern Matching

---
+++ C#/Intermediate/Code/switch1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Shape { }
+class Circle : Shape { }
+
+class Rectangle : Shape
+{
+    public double width = 100;
+    public double height = 100;
+}
+
+class Program
+{
+    public static void Draw(Shape s)
+    {
+        switch (s)
+        {
+            // 값을 조사 한다.
+            // const pattern matching
+            case null:
+                break;
+
+            // 해당 타입을 조사한다
+            // 캐스팅 까지 수행
+            // type pattern matching
+            case Circle c:
+                break;
+
+            //when을 통해 조건을 추가함.
+            case Rectangle r when r.width == r.height:
+                break;
+
+            // 해당 라인이 위의 case위에 있을 경우 위의 case는 실행이 안됨
+            // 일반적인 조건이 아래에 있어야함.
+            case Rectangle r:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public static void Main()
+    {
+        Draw(new Rectangle());
+
+        //# 전통적인 switch 문의 구조
+
+        int n = 1;
+        switch (n)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ C#/Intermediate/Code/switch_expression2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Shape { }
+
+class Rectangle : Shape
+{
+    public double Width { set; get; } = 10;
+    public double Height { set; get; } = 10;
+}
+class Circle : Shape
+{
+    public double Radius { set; get; } = 10;
+}
+
+class Point : Shape
+{
+    public double x = 0;
+    public double y = 0;
+
+    public void Deconstruct(out double ox, out double oy) => (ox, oy) = (x, y);
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Shape s = new Circle();
+
+        // type pattern matching
+        double area = s switch
+        {
+            null => 0,                                  // const pattern matching
+            Point _ => 0,                               // 필요 없는 변수는 _로 표기하면 생성 안함.
+            Circle c => Math.PI * c.Radius * c.Radius,
+            Rectangle r => r.Width * r.Height,
+            _ => 0
+        };
+
+        // tuple pattern
+        int value1 = 0;
+        int value2 = 0;
+
+        var ret1 = (value1, value2) switch
+        {
+            (0, 0) => 0,
+            var (a, b) when a > 100 => 100,
+            var (a, b) when a <= 100 && b > 100 => 200,
+            _ => 300
+        };
+
+
+        // positional pattern : Deconstructor 가 있는 타입
+        Point pt = new Point();
+        var (x1, y1) = pt;
+
+        var ret2 = pt switch
+        {
+            (0, 0) => 0,
+            var (a, b) when a > 100 => 100,
+            var (a, b) when a <= 100 && b > 100 => 200,
+            _ => 300
+        };
+    }
+}
