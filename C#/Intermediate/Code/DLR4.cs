929dcc419ad406f89c12fe73890f645c520d7c45
jsuk10 <jsuk10@naver.com>
2021-12-30 16:55:05 +0900

    DLR, Dynamic, 메서드 호출과 IL

---
+++ C#/Intermediate/Code/DLR4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        dynamic d = 10;
+
+        int a = d;         //ok
+        long b = d;         //ok
+        string s = d;       //예외 발생
+    }
+}
---
+++ C#/Intermediate/Code/stream_decorator1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+
+class Program
+{
+    public static void Main()
+    {
+        byte[] buff = new byte[1024 * 1024]; // 1M
+
+        FileStream fs = new FileStream("d:\\a.dat", FileMode.Create);
+
+        //Decorator
+        GZipStream gs = new GZipStream(fs, CompressionLevel.Fastest);
+
+        //Decorator에 Decorator추가
+        BufferedStream bs = new BufferedStream(gs);
+
+
+        //fs.Write(buff, 0, buff.Length);
+        bs.Write(buff, 0, buff.Length);
+        bs.Close();
+        gs.Close();
+        fs.Close();
+    }
+}
